64f6c9cec4c94326b4faad54bb89d4c2cfe82f7b
dchebayewski <vrij-nederland@yandex.com>
2021-04-11 00:24:15 +0200

    Updating documentation (signing)

--- scripts/upgrade_system.sh
+++ scripts/upgrade_system.sh
@@ -1,16 +1,17 @@
 #!/bin/sh
 #One-time action after first install fr: signing packages
-sign_packages=n
-if [ $sign_packages = "y" ]; then
+init_pacman=n
+if [ $init_pacman = "y" ]; then
   echo "signing packages"
   pacman-key --init
-  pacman-key --populate archlinuxarm
 fi
 #Arch linux update
 # update the package databases for each of the repositories on your system.
 pacman --sync --refresh
 # Update the local database of PGP keys used by the package maintainers. 
 # This step is optional, but may prevent problems later on with the upgrade if the database has not been updated for some time.
-pacman --sync --needed archlinux-keyring
+#pacman --sync --needed archlinux-keyring
+pacman --sync archlinuxarm-keyring
+pacman-key --populate archlinuxarm
 #Upgrade all system packages. Be sure to note the upgraded packages and any output that requires your attention during the upgrade process.
 pacman --sync --sysupgrade
