2b8530597d72d7e7f4610fa9c1e3f0fe74b2221f
dchebayewski <vrij-nederland@yandex.com>
2021-04-14 22:04:25 +0200

    Updating packages list

--- scripts/install_packages.sh
+++ scripts/install_packages.sh
@@ -5,7 +5,7 @@ pacman --sync mc
 pacman --sync samba
 pacman -S uboot-env uboot-mkimage
 pcaman --sync rsync
-pacman --sync pbzip2 powertop
+pacman --sync pbzip2 powertop htop
 echo "You might need to configure installed services"
 echo "Edit smb.conf *sudo nano /etc/samba/smb.conf*"
 echo "Enable the service unit: *sudo systemctl enable smb.service*"
